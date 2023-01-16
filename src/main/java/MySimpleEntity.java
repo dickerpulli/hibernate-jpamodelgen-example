c0ef2f1802a443007cc3b9e22ca746cd192148bb
Thomas Bosch <tbosch@gmx.de>
2023-01-16 15:48:58 +0100

    Create simple example

---
+++ src/main/java/MySimpleEntity.java
@@ -0,0 +1,31 @@
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class MySimpleEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/MySimpleRepository.java
@@ -0,0 +1,29 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class MySimpleRepository extends SimpleJpaRepository<MySimpleEntity, UUID> {
+
+    private EntityManager entityManager;
+
+    public MySimpleRepository(EntityManager entityManager) {
+        super(MySimpleEntity.class, entityManager);
+        this.entityManager = entityManager;
+    }
+
+    public List<MySimpleEntity> findByName(String name) {
+        var builder = entityManager.getCriteriaBuilder();
+        var query = builder.createQuery(MySimpleEntity.class);
+        var entity = query.from(MySimpleEntity.class);
+        query.select(entity)
+            .where(builder.equal(entity.get(MySimpleEntity_.name), name));
+        return entityManager.createQuery(query).getResultList();
+    }
+}
