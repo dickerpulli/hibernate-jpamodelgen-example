d1d46a8b18b3ce0389f8aaaa4eb4e64cc1b0d040
Thomas Bosch <tbosch@gmx.de>
2023-01-16 15:52:21 +0100

    Working example with Hibernate5

--- src/main/java/MySimpleRepository.java
+++ src/main/java/MySimpleRepository.java
@@ -1,7 +1,4 @@
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import javax.persistence.EntityManager;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Repository;
 
